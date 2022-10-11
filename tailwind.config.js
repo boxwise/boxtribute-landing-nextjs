04d9840f174e09e20049238a1518a28e3a372ca1
HaGuesto <hans@boxtribute.org>
2022-10-11 16:49:50 +0200

    transfer tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./components/**/*.tsx', './pages/**/*.tsx'],
+  purge: ['./components/**/*.tsx', './pages/**/*.tsx'],
   theme: {
     extend: {
       colors: {
-        'accent-1': '#FAFAFA',
-        'accent-2': '#EAEAEA',
-        'accent-7': '#333',
+        red: '#EF4049',
+        navy: '#29335F',
+        blue: '#ABCFE3',
+        gray: '#EAEAED',
         success: '#0070f3',
         cyan: '#79FFE1',
+      },
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
       },
       spacing: {
         28: '7rem',
@@ -20,16 +26,14 @@ module.exports = {
         tight: 1.2,
       },
       fontSize: {
+        'md':'1rem',
+        'xl': '1.25',
+        '2xl': '1.5rem',
         '5xl': '2.5rem',
         '6xl': '2.75rem',
         '7xl': '4.5rem',
-        '8xl': '6.25rem',
-      },
-      boxShadow: {
-        sm: '0 5px 10px rgba(0, 0, 0, 0.12)',
-        md: '0 8px 30px rgba(0, 0, 0, 0.12)',
+        '8xl': '7rem',
       },
     },
   },
-  plugins: [],
 }
