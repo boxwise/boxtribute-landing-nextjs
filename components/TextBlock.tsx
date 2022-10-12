1a6ad5e54c36e9e8ac1dfce9a971d8b56a3462c0
HaGuesto <hans@boxtribute.org>
2022-10-12 09:17:13 +0200

    transfer TextBlock

---
+++ components/TextBlock.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react'
+
+type Props = {
+  children?: ReactNode
+  bg_color?: string
+  text_color?: string
+  text_justify: "center" | "left"
+}
+
+// TODO: Why is a text block h3?
+const TextBlock = ({ children, bg_color, text_color, text_justify }: Props) => {
+  return (
+    <h3 className={`p-4 lg:p-20 bg-${bg_color} text-${text_color} text-${text_justify}`}>
+      {children}
+    </h3>
+    
+  )
+}
+
+export default TextBlock
