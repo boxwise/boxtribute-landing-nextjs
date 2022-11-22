bd29fe6994ecfd9885e4bc45744740134f37db06
HaGuesto <hans@boxtribute.org>
2022-11-22 21:59:00 +0100

    Fix Avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Avatar = ({ name, picture }: IAuthor) => {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center gap-2">
       <Image src={picture} width="48px" height="48px" className="rounded-full mr-4" alt={name} />
       <div className="text-xl font-bold">{name}</div>
     </div>
