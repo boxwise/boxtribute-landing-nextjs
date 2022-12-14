6d93bb03180ed53989ce61d2cbb134e260ae02a6
Roanna K <6267028+aerinsol@users.noreply.github.com>
2022-12-15 04:08:54 +0800

    Manual URL change to CTA for Winter Fundraiser
    From: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=A8P3SYKKUPMPN&source=url
    
    to: https://donorbox.org/boxtribute-fundraising-campaign

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -47,7 +47,8 @@ export const Navbar = () => {
           <li className="list-none" key="CTA">
             <CTAButton
               ctaLink={
-                "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=A8P3SYKKUPMPN&source=url"
+                // This is the Donate URL for the Winter Fundraiser - needs to be changed back to Paypal afterwards
+                "https://donorbox.org/boxtribute-fundraising-campaign"
               }
               ctaButton={"Donate"}
               noMdBreak={true}
