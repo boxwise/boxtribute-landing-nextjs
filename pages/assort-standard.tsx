7281f5bc8e99f36bf2549b02896215a395022035
Philipp Metzner <pmetzner@freenet.de>
2026-06-15 10:08:27 +0200

    less-margin

--- pages/assort-standard.tsx
+++ pages/assort-standard.tsx
@@ -396,7 +396,7 @@ export const AssortStandard = ({ footerData }: IProps) => {
       </section>
 
       <section className="bg-white">
-        <div className="container mx-auto px-4 md:px-16 py-10 md:py-14">
+        <div className="p-8 md:p-12 lg:p-16">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
             <div>
               <h3 className="mb-4">Explore ASSORT</h3>
@@ -429,7 +429,7 @@ export const AssortStandard = ({ footerData }: IProps) => {
       </section>
 
       <section className="bg-white border-t border-gray-100">
-        <div className="container mx-auto px-4 md:px-16 py-8 md:py-12">
+        <div className="p-8 md:p-12 lg:p-16">
           <AssortTable products={products} />
         </div>
       </section>
