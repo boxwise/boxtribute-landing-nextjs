357591643117675543edfa96ae2ab870b18ee2e1
Philipp Metzner <pmetzner@freenet.de>
2026-08-12 10:46:17 +0200

    fix-build

--- components/CTAButton.tsx
+++ components/CTAButton.tsx
@@ -12,7 +12,7 @@ const CTAButton = ({ ctaLink, ctaButton, noMdBreak }: Props) => {
       {/* move styling in variables to also put CTA button in navbar */}
       <button
         data-umami-event="cta-click"
-        data-umami-event-cta={`${cta_button}`}
+        data-umami-event-cta={`${ctaButton}`}
         className={`bg-red px-4 py-1 text-white text-xl rounded-md ${
           !noMdBreak ? "md:px-8 md:py-2 md:text-2xl md:rounded-lg lg:text-3xl" : ""
         }`}
