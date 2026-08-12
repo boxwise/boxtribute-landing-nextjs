388ca6fed7762eed740cdc07b7fdad48cf263f5d
Philipp Metzner <pmetzner@freenet.de>
2026-08-12 10:24:33 +0200

    Data attributes for CTA click

--- components/CTAButton.tsx
+++ components/CTAButton.tsx
@@ -11,7 +11,8 @@ const CTAButton = ({ ctaLink, ctaButton, noMdBreak }: Props) => {
     <Link href={ctaLink}>
       {/* move styling in variables to also put CTA button in navbar */}
       <button
-        data-umami-event={`CTA Click - ${ctaButton}`}
+        data-umami-event="cta-click"
+        data-umami-event-cta={`${cta_button}`}
         className={`bg-red px-4 py-1 text-white text-xl rounded-md ${
           !noMdBreak ? "md:px-8 md:py-2 md:text-2xl md:rounded-lg lg:text-3xl" : ""
         }`}
