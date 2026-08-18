122bb021732ee37a0d0063b2686dfa3122fb3c9a
Philipp Metzner <pmetzner@freenet.de>
2026-08-18 10:15:27 +0200

    inactive-marker

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -23,14 +23,18 @@ const Map = ({ center, baseMarkers }: IMapContainerData) => {
     weight: 4,
   };
 
-  const markerIcon = L.divIcon({
-    html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#EF4049" width="36" height="36" aria-hidden="true">
-      <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
-    </svg>`,
-    className: "",
-    iconSize: [36, 36],
-    iconAnchor: [18, 36],
-  });
+  const getMarkerIcon = (status?: "active" | "inactive") => {
+    const color = status === "inactive" ? "#964B00" : "#EF4049";
+    return L.divIcon({
+      html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${color}" width="36" height="36" aria-hidden="true">
+        <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827[...]
+        </svg>`,
+      className: "",
+      iconSize: [36, 36],
+      iconAnchor: [18, 36],
+    });
+  };
+
   const isBreakpoint = useMediaQuery(768);
 
   return (
@@ -56,7 +60,7 @@ const Map = ({ center, baseMarkers }: IMapContainerData) => {
         }
       />
       {baseMarkers.map((marker, i) => (
-        <Marker key={i} icon={markerIcon} position={[marker.position.lat, marker.position.long]}>
+        <Marker key={i} icon={getMarkerIcon(maker.status)} position={[marker.position.lat, marker.position.long]}>
           <Popup
             maxWidth={isBreakpoint ? 200 : 400}
             maxHeight={400}
