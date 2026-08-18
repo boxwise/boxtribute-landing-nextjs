3f51aef24523a8c7f0d045003874c04dac7ab466
Philipp Metzner <pmetzner@freenet.de>
2026-08-18 11:14:50 +0200

    update

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -24,7 +24,7 @@ const Map = ({ center, baseMarkers }: IMapContainerData) => {
   };
 
   const getMarkerIcon = (status?: "active" | "inactive") => {
-    const color = status === "inactive" ? "#964B00" : "#EF4049";
+    const color = status === "inactive" ? "#D89016" : "#EF4049";
     return L.divIcon({
       html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${color}" width="36" height="36" aria-hidden="true">
         <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.847 2.019 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
@@ -75,7 +75,8 @@ const Map = ({ center, baseMarkers }: IMapContainerData) => {
                 color: "inherit",
               }}
             >
-              <img src={marker.logo} className="h-32 w-full object-contain" />
+              <img src={marker.logo} className="h-32" />
+              {marker.status === "inactive" && (<h5><em>Ended deployment</em></h5>)}
               <h3>{marker.base_name}</h3>
               <h4 className="bg-lightblue">{marker.organisation}</h4>
             </a>
