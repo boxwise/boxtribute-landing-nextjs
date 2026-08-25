ece404f8013b96dac33b88031aaef5c5b0a96fb6
Philipp Metzner <pmetzner@freenet.de>
2026-08-25 08:52:08 +0200

    usePlausible

--- components/CTAButton.tsx
+++ components/CTAButton.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { usePlausible } from "next-plausible";
 
 type Props = {
   ctaLink: string;
@@ -7,13 +8,15 @@ type Props = {
 };
 
 const CTAButton = ({ ctaLink, ctaButton, noMdBreak }: Props) => {
+  const plausible = usePlausible();
   return (
     <Link href={ctaLink}>
       {/* move styling in variables to also put CTA button in navbar */}
       <button
         className={`bg-red px-4 py-1 text-white text-xl rounded-md ${
           !noMdBreak ? "md:px-8 md:py-2 md:text-2xl md:rounded-lg lg:text-3xl" : ""
-        } plausible-event-name=Button+${ctaButton.replace(" ", "+")}`}
+        }`}
+        onClick={() => plausible("button-click", { props : { id : ctaButton.toLowerCase().replace(" ", "-") } })}
       >
         {ctaButton}
       </button>
